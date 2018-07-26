b6ff8eb03c315cf517972bad8109bf00e4b537f5
jirapat <jirapat.lpn@gmail.com>
2018-07-26 19:34:38 +0700

    homework

--- temperature.test/UnitTest1.cs
+++ temperature.test/UnitTest1.cs
@@ -8,15 +8,22 @@ namespace temperature.test {
     public class UnitTest1 {
         [Theory (DisplayName = "เปลี่ยนค่า องศา Fahrenheit ให้เป็น Celsius ได้สำเร็จ")]
         [InlineData (32, 0)]
+        [InlineData (50, 10)]
+        [InlineData (40, 4.4444444444444446)]
+        [InlineData (40.5, 4.7222222222222223)]
+        [InlineData (89.6, 32)]
         public void ConvertFahrenheitToCelsiusSuccess (double input, double expectedResult) {
-            var cal = new TempCalculator ();
-            var x = cal.ConvertFahrenheitToCelsius (input);
+            var cal = new TempCalculator();
+            var x = cal.ConvertFahrenheitToCelsius(input);
 
-            x.Should ().Be (expectedResult);
+            x.Should().Be (expectedResult);
         }
 
         [Theory (DisplayName = "เปลี่ยนค่า องศา Kelvin ให้เป็น Celsius ได้สำเร็จ")]
         [InlineData (363.15, 90)]
+        [InlineData (365.15, 92)]
+        [InlineData (365, 91.850000000000023)]
+        [InlineData (365.5, 92.350000000000023)]
         public void ConvertKelvinToCelsiusSuccess (double input, double expectedResult) {
             var cal = new TempCalculator ();
             var x = cal.ConvertKelvinToCelsius (input);
@@ -26,6 +33,9 @@ namespace temperature.test {
 
         [Theory (DisplayName = "เปลี่ยนค่า องศา celsius ให้เป็น Fahrenheit ได้สำเร็จ")]
         [InlineData (90, 194)]
+        [InlineData (100, 212)]
+        [InlineData (94, 201.20000000000002)]
+        [InlineData (94.5, 202.1)]
         public void ConvertCelsiusToFahrenheitSuccess (double input, double expectedResult) {
             var cal = new TempCalculator ();
             var x = cal.ConvertCelsiusToFahrenheit (input);
@@ -35,6 +45,9 @@ namespace temperature.test {
 
         [Theory (DisplayName = "เปลี่ยนค่า องศา Kelvin ให้เป็น Fahrenheit ได้สำเร็จ")]
         [InlineData (90, -297.67)]
+        [InlineData (95, -288.67)]
+        [InlineData (95.5, -287.77)]
+        [InlineData (-96, -632.47)]
         public void ConvertKelvinToFahrenheitSuccess (double input, double expectedResult) {
             var cal = new TempCalculator ();
             var x = cal.ConvertKelvinToFahrenheit (input);
@@ -44,6 +57,9 @@ namespace temperature.test {
 
         [Theory (DisplayName = "เปลี่ยนค่า องศา Fahrenheit ให้เป็น Kelvin ได้สำเร็จ")]
         [InlineData (-297.67, 90)]
+        [InlineData (288.67, 415.74444444444447)]
+        [InlineData (-632.47, -96)]
+        [InlineData (-287.77, 95.500000000000014)]
         public void ConvertFahrenheitToKelvinSuccess (double input, double expectedResult) {
             var cal = new TempCalculator ();
             var x = cal.ConvertFahrenheitToKelvin (input);
@@ -51,8 +67,11 @@ namespace temperature.test {
             x.Should ().Be (expectedResult);
         }
 
-        [Theory (DisplayName = "เปลี่ยนค่า องศา celsius ให้เป็น Fahrenheit ได้สำเร็จ")]
+        [Theory (DisplayName = "เปลี่ยนค่า องศา celsius ให้เป็น Kelvin ได้สำเร็จ")]
         [InlineData (90, 363.15)]
+        [InlineData (92, 365.15)]
+        [InlineData (91.850000000000023, 365)]
+        [InlineData (92.350000000000023, 365.5)]
         public void ConvertCelsiusToKelvinSuccess (double input, double expectedResult) {
             var cal = new TempCalculator ();
             var x = cal.ConvertCelsiusToKelvin (input);
--- temperature/Calculator.cs
+++ temperature/Calculator.cs
@@ -1,7 +1,9 @@
 using System;
 
-namespace temperature {
-    public class TempCalculator {
+namespace temperature
+{
+    public class TempCalculator
+    {
 
         #region สูตรการแปลงอุณหภูมิ
         // °F = (1.8 × °C) + 32
@@ -18,8 +20,10 @@ namespace temperature {
         /// </summary>
         /// <param name="fahrenheit"></param>
         /// <returns></returns>
-        public double ConvertFahrenheitToCelsius (double fahrenheit) {
-            throw new NotImplementedException ();
+        public double ConvertFahrenheitToCelsius(double fahrenheit)
+        {
+            var Celsius = (fahrenheit - 32.0) * (5.0 / 9.0);
+            return Celsius;
         }
         /// <summary>
         /// เปลี่ยนค่า องศา Kelvin ให้เป็น Celsius
@@ -27,8 +31,10 @@ namespace temperature {
         /// </summary>
         /// <param name="kelvin"></param>
         /// <returns></returns>
-        public double ConvertKelvinToCelsius (double kelvin) {
-            throw new NotImplementedException ();
+        public double ConvertKelvinToCelsius(double kelvin)
+        {
+            var Celsius = kelvin - 273.15;
+            return Celsius;
         }
 
         /// <summary>
@@ -37,8 +43,10 @@ namespace temperature {
         /// </summary>
         /// <param name="celsius"></param>
         /// <returns></returns>
-        public double ConvertCelsiusToFahrenheit (double celsius) {
-            throw new NotImplementedException ();
+        public double ConvertCelsiusToFahrenheit(double celsius)
+        {
+            var Fahrenheit = (celsius * 1.8) + 32.0;
+            return Fahrenheit;
         }
         /// <summary>
         /// เปลี่ยนค่า องศา Kelvin ให้เป็น Fahrenheit
@@ -46,8 +54,10 @@ namespace temperature {
         /// </summary>
         /// <param name="Kelvin"></param>
         /// <returns></returns>
-        public double ConvertKelvinToFahrenheit (double kelvin) {
-            throw new NotImplementedException ();
+        public double ConvertKelvinToFahrenheit(double kelvin)
+        {
+            var Fahrenheit = (kelvin * 1.8) - 459.67;
+            return Fahrenheit;
         }
         /// <summary>
         /// เปลี่ยนค่า องศา Fahrenheit ให้เป็น Kelvin
@@ -55,8 +65,10 @@ namespace temperature {
         /// </summary>
         /// <param name="fahrenheit"></param>
         /// <returns></returns>
-        public double ConvertFahrenheitToKelvin (double fahrenheit) {
-            throw new NotImplementedException ();
+        public double ConvertFahrenheitToKelvin(double fahrenheit)
+        {
+            var Kelvin = (fahrenheit + 459.67) / 1.8 ;
+            return Kelvin;
         }
         /// <summary>
         /// เปลี่ยนค่า องศา celsius ให้เป็น Kelvin
@@ -64,8 +76,10 @@ namespace temperature {
         /// </summary>
         /// <param name="celsius"></param>
         /// <returns></returns>
-        public double ConvertCelsiusToKelvin (double celsius) {
-            throw new NotImplementedException ();
+        public double ConvertCelsiusToKelvin(double celsius)
+        {
+            var Kelvin =  celsius + 273.15;
+            return Kelvin;
         }
 
     }
